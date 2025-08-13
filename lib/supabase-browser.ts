c9a2fce07ea711bf40734933e4dcfc35011846d9
talormayde <tb@freefam.casa>
2025-08-13 14:02:01 -0500

    Update supabase-browser.ts

--- lib/supabase-browser.ts
+++ lib/supabase-browser.ts
@@ -1,7 +1,7 @@
-import { createBrowserClient } from '@supabase/auth-helpers-nextjs';
+import { createClient } from '@supabase/supabase-js';
 
 export function supabaseBrowser() {
-  return createBrowserClient(
+  return createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   );
