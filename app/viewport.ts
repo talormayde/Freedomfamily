75b1cf245e068f3790c630e7f2564b894e22404d
Talor <tb@freefam.casa>
2025-08-17 20:11:36 +0000

    Create viewport.ts

---
+++ app/viewport.ts
@@ -0,0 +1,11 @@
+// app/viewport.ts
+export const viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  viewportFit: 'cover',
+  themeColor: [
+    { media: '(prefers-color-scheme: light)', color: '#ffffff' },
+    { media: '(prefers-color-scheme: dark)', color: '#0a0a0a' },
+  ],
+};
